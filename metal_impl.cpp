8553c5879c8155103bda36183bd134cfb72b555c
Aagamir <mikimarcinkowski@gmail.com>
2026-06-07 12:50:22 +0200

    finalizowanie projektu

---
+++ metal_impl.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Mikołaj Marcinkowski on 26/04/2026.
+//
+#define NS_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+
+#include <Foundation/Foundation.hpp>
+#include <Metal/Metal.hpp>
+#include <QuartzCore/QuartzCore.hpp>
